8566ddfed9d5806662c57b4368aca8296d294796
hubbell.64 <hubbell.64@cse-std8.coeit.osu.edu>
2019-01-25 17:55:39 -0500

    Got rid of initialize and getter methods. Replaced with attr_accessor.

--- card.rb
+++ card.rb
@@ -1,27 +1,5 @@
 class Card
 
-	# initialized to nil values until we figure out what to initialize to.
-	def initialize
-		@color = nil
-		@shape = nil
-		@shading = nil
-		@number = nil
-	end
-
-	def getColor
-		@color # implicit return
-	end
-
-	def getShape
-		@shape # implicit return
-	end
-
-	def getShading
-		@shading # implicit return
-	end
-
-	def getNumber
-		@number # implicit return
-	end
+	attr_accessor :color, :shape, :shading, :number
 
 end
