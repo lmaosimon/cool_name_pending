eb3b0699ef7c7cc4a473a11cdabe6d212697df66
hubbell.64 <hubbell.64@cse-std8.coeit.osu.edu>
2019-01-25 15:47:02 -0500

    Created initial file for card class

---
+++ card.rb
@@ -0,0 +1,27 @@
+class Card
+
+	# initialized to nil values until we figure out what to initialize to.
+	def initialize
+		@color = nil
+		@shape = nil
+		@shading = nil
+		@number = nil
+	end
+
+	def getColor
+		@color # implicit return
+	end
+
+	def getShape
+		@shape # implicit return
+	end
+
+	def getShading
+		@shading # implicit return
+	end
+
+	def getNumber
+		@number # implicit return
+	end
+
+end
