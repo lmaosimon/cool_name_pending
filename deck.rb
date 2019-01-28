2993f7da42f58a7078d0bcad032dc449888a3b3d
hubbell.64 <hubbell.64@cse-std3.coeit.osu.edu>
2019-01-28 11:41:32 -0500

    created initialize and shuffle methods for deck class

---
+++ deck.rb
@@ -0,0 +1,31 @@
+class Deck
+
+	# Think about algorithm for figuring out if a table has a set on it.
+	# Also think about an algorithm for checking if three selected cards make up a set.
+
+	def initialize
+		@deck = []
+
+		i = 0
+
+		for color in [0..2]
+			for shape in [0..2]
+				for shading in [0..2]
+					for number in [0..2]
+						@deck[i] = Card.new
+						@deck[i].color = color
+						@deck[i].shape = shape
+						@deck[i].shading = shading
+						@deck[i].number = number
+						i += 1
+					end
+				end
+			end
+		end
+	end
+
+	def shuffle
+		@deck.shuffle
+	end
+
+end
