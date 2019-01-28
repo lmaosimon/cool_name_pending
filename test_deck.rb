69637626848459f64ba1c969b1a8dced08494632
hubbell.64 <hubbell.64@cse-std3.coeit.osu.edu>
2019-01-28 17:13:49 -0500

    fixed for in loops in test file

--- test_deck.rb
+++ test_deck.rb
@@ -8,14 +8,14 @@ puts "#{classType1}"
 classType2 = d.deck[0].class
 puts "#{classType2}"
 
-for i in [0..11]
+for i in 0..11
 	c = d.deck[i]
-	puts "#{c.class}"
+	puts "#{c.color}"
 end
 
 d.shuffle
 
-for i in [0..11]
+for i in 0..11
 	c = d.deck[i]
-	puts "#{c.class}"
+	puts "#{c.color}"
 end
