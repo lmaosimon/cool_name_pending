5b239dd88718b8da3d2c25a5625170bd3a5e5438
hubbell.64 <hubbell.64@cse-std3.coeit.osu.edu>
2019-01-28 12:49:25 -0500

    updated test file for deck and card classes

--- test_deck.rb
+++ test_deck.rb
@@ -2,8 +2,10 @@ require "./deck.rb"
 
 d = Deck.new
 
-classType = d.class
-puts "#{classType}"
+classType1 = d.class
+puts "#{classType1}"
+classType2 = d.deck[0].class
+puts "#{classType2}"
 
 for i in [0..11]
 	puts "#{d.deck[i].color}"
