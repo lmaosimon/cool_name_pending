0897aa66403875e6bedf316849364d8166e18fa7
pat-hubbs <hubbell.64@osu.edu>
2019-02-18 22:22:42 -0500

    Created function to retrieve table data.

--- project3/scraper.rb
+++ project3/scraper.rb
@@ -1,5 +1,14 @@
 require "mechanize"
 
+def getTableInfo(table)
+	bookHash = Hash.new
+	entries = table.css(".bibItemsEntry td")
+	bookHash["location"] = entries[0].text
+	bookHash["status"] = entries[3].text
+	return bookHash
+end
+
+
 url = "https://library.osu.edu"
 
 agent = Mechanize.new
@@ -22,11 +31,17 @@ pp searchInput
 page = agent.submit(searchInput);
 
 test = page.css(".save").text
+test2 = page.css("a")
+puts test2.class
 
-if test != ""
+if !test.empty?
+	bookHash = Hash.new
 	puts "You're on a book page!"
+	table = page.css(".bibItems");
+	bookHash = getTableInfo(table);
 elsif
 	puts "You're on a list page!"
+	a = [] #Array to hold book hashes.
 end
 
 searchLinks = page.links
