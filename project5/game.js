54ff67b2a80a17403647d9ed071a6462e2a23ce2
Patrick Hubbell <hubbell.64@osu.edu>
2019-03-22 19:23:49 -0400

    Merging

--- project5/game.js
+++ project5/game.js
@@ -1,26 +1,3 @@
-var numCards = 12;
-
-var deck = [];
-
-document.getElementById("reset").addEventListener("click", function(){
-    document.getElementById("message").innerHTML = "You have reset the game.";
-});
-
-document.getElementById("hint").addEventListener("click", function(){
-    document.getElementById("message").innerHTML = "Here is a hint.";
-});
-
-function createCardListeners() {
-
-    var cards = document.querySelectorAll(".card");
-    for (var i = 0; i < cards.length; i++) {
-        cards[i].addEventListener("click", function() {
-            this.classList.toggle("selected");
-        });
-    }
-
-}
-
 var colorEnum = {
     RED = 0,
     GREEN = 1,
@@ -65,6 +42,29 @@ var numberEnum = {
     }
 }
 
+var numCards = 12;
+
+var deck = [];
+
+document.getElementById("reset").addEventListener("click", function(){
+    document.getElementById("message").innerHTML = "You have reset the game.";
+});
+
+document.getElementById("hint").addEventListener("click", function(){
+    document.getElementById("message").innerHTML = "Here is a hint.";
+});
+
+function createCardListeners() {
+
+    var cards = document.querySelectorAll(".card");
+    for (var i = 0; i < cards.length; i++) {
+        cards[i].addEventListener("click", function() {
+            this.classList.toggle("selected");
+        });
+    }
+
+}
+
 function createDeck(deck) {
 
     for (var i = 0; i <= 2; i++) {
@@ -85,5 +85,6 @@ function createDeck(deck) {
 
 }
 
+createCardListeners();
 createDeck(deck);
 console.log(deck);
