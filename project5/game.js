65099ab4624c0878972f9c4bdb6513226403d3e7
Patrick Hubbell <hubbell.64@osu.edu>
2019-03-22 18:04:38 -0400

    Made a create deck function

--- project5/game.js
+++ project5/game.js
@@ -1,6 +1,8 @@
 var cards = document.querySelectorAll(".card");
 var numCards = 12;
 
+var deck = [];
+
 document.getElementById("reset").addEventListener("click", function(){
     document.getElementById("message").innerHTML = "You have reset the game.";
 });
@@ -14,3 +16,25 @@ for (var i = 0; i < cards.length; i++) {
         this.classList.add("selected");
     });
 }
+
+function createDeck(deck) {
+
+    for (var i = 0; i <= 2; i++) {
+        for (var j = 0; j <= 2; j++) {
+            for (var k = 0; k <= 2; k++) {
+                for (var l = 0; l <= 2; l++) {
+                    var card = { color: i,
+                                 shape: j,
+                                 size: k,
+                                 number: l };
+                    deck.push(card);
+                }
+            }
+        }
+    }
+
+}
+                
+
+createDeck(deck);
+console.log(deck);
