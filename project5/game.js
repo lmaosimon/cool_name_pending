a7d81eee30963e54f97dc41ce382d40309af57c2
Sean <seanmbower28@gmail.com>
2019-03-22 18:28:59 -0400

    Modified each png file name to match the desired naming scheme. Created ENUM variables to assign cards to their correlating png file

--- project5/game.js
+++ project5/game.js
@@ -14,3 +14,47 @@ for (var i = 0; i < cards.length; i++) {
         this.classList.add("selected");
     });
 }
+
+var colorEnum = {
+    RED = 0,
+    GREEN = 1,
+    BLUE = 2,
+    properties: {
+        0: {name: "RED", value: "0", code: "R"},
+        1: {name: "GREEN", value: "1", code: "G"},
+        2: {name: "BLUE", value: "2", code: "B"}
+    }
+}
+
+var shapeEnum = {
+    CIRCLE = 0,
+    SQUARE = 1,
+    TRIANGLE = 2,
+    properties: {
+        0: {name: "CIRCLE", value: "0", code: "C"},
+        1: {name: "SQUARE", value: "1", code: "S"},
+        2: {name: "TRIANGLE", value: "2", code: "T"}
+    }
+}
+
+var shadeEnum = {
+    EMPTY = 0,
+    FILLED = 1,
+    STRIPE = 2,
+    properties: {
+        0: {name: "EMPTY", value: "0", code: "E"},
+        1: {name: "FILLED", value: "1", code: "F"},
+        2: {name: "STRIPE", value: "2", code: "S"}
+    }
+}
+
+var numberEnum = {
+    SINGLE = 0,
+    DOUBLE = 1,
+    TRIO = 0,
+    properties: {
+        0: {value: "0", code: "1"},
+        1: {value: "1", code: "2"},
+        2: {value: "2", code: "3"}
+    }
+}
