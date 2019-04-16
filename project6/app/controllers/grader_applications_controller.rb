b4079c3773fa34c47572ed00324e976362116695
ginodetore <detore.4@osu.edu>
2019-04-16 17:52:22 -0400

    App controller updates

--- project6/app/controllers/grader_applications_controller.rb
+++ project6/app/controllers/grader_applications_controller.rb
@@ -1,9 +1,57 @@
 class GraderApplicationsController < ApplicationController
-
+  before_action :logged_in_student, only: [:new, :index, :create, :destroy, :edit, :update ]
+  before_action :correct_user, only: [:new, :index, :create, :destroy, :edit, :update ]
+  
   def new
-    @application = GraderApplication.new
+    @applications = GraderApplication.new
   end
 
-  
+  def index
+    @applications = GraderApplication.all
+  end
+
+  def create
+    @application = GraderApplication.new(application_params);
+    if @application.save
+      flash[:success] = "Application successfully submitted!"
+      current_user.grader_application << @application;
+      redirect_to current_user; #FIXME:
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+    def application_params
+      params.require(:grader_application).permit(:name, :email, :qualifications);
+    end
+
+    def logged_in_student
+      unless logged_in? && isStudent?(current_user)
+        if (!isStudent?(current_user))
+          flash[:danger] = "Only students may apply for grader positions.";
+          redirect_to current_user;
+        else
+          flash[:danger] = "You must be logged in as a student to view this page."
+          redirect_to login_url
+        end
+      end
+    end
+
+    def correct_user
+      redirect_to(current_user) unless current_user?(current_user)
+    end
 
 end
--- project6/config/routes.rb
+++ project6/config/routes.rb
@@ -11,7 +11,9 @@ Rails.application.routes.draw do
   get '/newcourse', to: 'courses#new'
   get '/courses', to: 'courses#index'
   get '/allcourses', to: 'courses#admin_index'
+  get '/newapplication', to: 'grader_applications#new'
   resources :users
   resources :courses
+  resources :grader_applications
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
