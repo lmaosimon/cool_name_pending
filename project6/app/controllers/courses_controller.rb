cd6ff5865c89ded4f56ddbde9eb83b3da131e123
pat-hubbs <hubbell.64@osu.edu>
2019-04-15 21:24:10 -0400

    Preparing to push course changes to master

--- project6/app/controllers/courses_controller.rb
+++ project6/app/controllers/courses_controller.rb
@@ -18,6 +18,26 @@ class CoursesController < ApplicationController
     end
   end
 
+  def destroy
+    Course.find(params[:id]).destroy;
+    flash[:success] = "Course deleted";
+    redirect_to courses_url;
+  end
+
+  def edit
+    @course = Course.find(params[:id]);
+  end
+
+  def update
+    @course = Course.find(params[:id]);
+    if (@course.update_attributes(course_params))
+      flash[:success] = "Course updated";
+      redirect_to courses_url;
+    else
+      render 'edit'
+    end
+  end
+
   private
     def course_params
       params.require(:course).permit(:instructor, :email, :course_name, :section, :monday, :tuesday,
