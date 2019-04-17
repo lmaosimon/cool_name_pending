a2727582bdb9e7b2df3b5ead05c73f966dd268a3
pat-hubbs <hubbell.64@osu.edu>
2019-04-17 18:12:20 -0400

    getting ready to merge origin/master

--- project6/app/assets/javascripts/grader_application.js
+++ project6/app/assets/javascripts/grader_application.js
@@ -1,10 +1,8 @@
-$(document).ready(function() { 
-     
-       $('.btn.submit').attr("disabled",true);
-     
-       $('input[type="checkbox"]').change(function() {
-          $('.btn.submit').attr('disabled', $('input[type="checkbox"]:checked').length == 0);
-       });
+$(document).on('turbolinks:load', function() { 
+ 
+   $('input[type="checkbox"]').change(function() {
+      $('.btn.submit').attr('disabled', $('input[type="checkbox"]:checked').length == 0);
+   });
      
 });
 
