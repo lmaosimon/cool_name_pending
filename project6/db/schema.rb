40616e7f5154c0e7464ad89fbcdc28a0502a4064
pat-hubbs <hubbell.64@osu.edu>
2019-04-17 20:50:35 -0400

    Navbar fix

--- project6/db/schema.rb
+++ project6/db/schema.rb
@@ -67,9 +67,7 @@ ActiveRecord::Schema.define(version: 2019_04_17_224301) do
     t.string "password_digest"
     t.string "status"
     t.boolean "admin", default: false
-    t.integer "grader_applications_id"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["grader_applications_id"], name: "index_users_on_grader_applications_id"
   end
 
 end
