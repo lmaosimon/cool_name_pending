3c8b098c3203ab38c50256a3b2bce5e57ccdf1ff
pat-hubbs <hubbell.64@osu.edu>
2019-04-28 21:44:39 -0400

    Added comments to all user related files

--- project6/app/controllers/users_controller.rb
+++ project6/app/controllers/users_controller.rb
@@ -1,17 +1,26 @@
 class UsersController < ApplicationController
+  # Only allow a logged in user/the correct user to view their profile page
   before_action :logged_in_user, only: [:show]
   before_action :correct_user, only: [:show]
+
+  # Controller action for the log in page
   def new
+    # Create a new user
     @user = User.new;
   end
 
+  # Controller action for the user's profile page
   def show
+    # Get the correct user via user id in params hash
     @user = User.find(params[:id]);
   end
 
+  # Controller action for the sign up page
   def create
+    # Create a new user with strong parameters via user_params private method
     @user = User.new(user_params);
     if @user.save
+      # Log user into their session via log_in function in sessions_helper.rb
       log_in(@user);
       flash[:success] = "Welcome to the OSU CSE Grader's Portal!"
       redirect_to @user;
@@ -20,15 +29,26 @@ class UsersController < ApplicationController
     end
   end
 
+  # Controller action to update user attributes
+  # This action only occurs when the admin assigns a course to a student user
   def update
+
+    # Delete password attribute from hash so update_attributes function below
+    # does not attempt to update the password, leads to errors otherwise.
     if (params[:password].blank?)
       params.delete(:password);
     end
+
+    # Find the student user whose assignment attribute is changing
     @user = User.find(params[:id]);
+
+    # Update their attributes
     if (@user.update_attributes(user_params))
       flash[:success] = "User assigned to course";
+      # Give the student their assignment
       @user.grader_application.assignment = course_info(@user.course);
       @user.grader_application.save;
+      # Set the assigned attribute of the course they were assigned to to true
       c = Course.find(@user.course_id);
       c.assigned = true;
       c.save;
@@ -40,10 +60,15 @@ class UsersController < ApplicationController
   end
 
   private
+
+    # To prevent unwanted/malicious data being passed to User.new when a new user is being created,
+    # this strong parameters function is used to specify which which parameters are required and
+    # which ones are permitted
     def user_params
       params.require(:user).permit(:id, :name, :email, :password, :password_confirmation, :status, :course_id);
     end
 
+    # Function to check if current user is logged in, else redirect to the login page
     def logged_in_user
       unless logged_in?
         flash[:danger] = "Please log in.";
@@ -51,6 +76,9 @@ class UsersController < ApplicationController
       end
     end
 
+    # Function to check if the correct user is accessing a page corresponding to their user
+    # account. If they try to access a page belonging to another user, they are redirected to
+    # their own profile page
     def correct_user
       if (User.exists?(params[:id]))
         @user = User.find(params[:id]);
--- project6/app/models/user.rb
+++ project6/app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
-    has_many :recommendations, dependent: :destroy # Can have many submitted recommendations if status is Faculty Employee
+
+    # Can have many submitted recommendations if user status is Employee
+    has_many :recommendations, dependent: :destroy
+    # Can have one grader application if user status is Student
     has_one :grader_application, required: false, dependent: :destroy
-    has_many :courses, dependent: :destroy # Can have many courses if status is Faculty Employee
+    # Can have many courses if user status is Employee
+    has_many :courses, dependent: :destroy
+    # Belongs to a course if a student user is assigned to a course
     belongs_to :course, required: false
+
     before_save { self.email = email.downcase }
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[a-zA-Z]+\.\d+@[oO][sS][uU]\.[eE][dD][uU]\z/;
@@ -11,30 +17,62 @@ class User < ApplicationRecord
                       uniqueness: { case_sensitive: false }
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }, :if => :password
-    validates :status, presence: true   
+    validates :status, presence: true 
+    # Custom validation, explained below
     validate :status_auth
 
     private
+        # Custom validation function to make sure when creating a user account that the user is an
+        # actual OSU student or employee via their name.number, and to make sure that a student
+        # isn't signing up as a teacher, and a teacher isn't signing up as a student. Uses mechanize.
+        # NOTE: We know that students that have jobs on campus have a 'Student Employee' affilation,
+        # so they can technically sign up as a teacher. But, we had to be able to allow someone with
+        # that affilation to sign up as a teacher because, for example, a Systems 2 teacher,
+        # Jayson Boubin, is a grad student so the only Employee affiliation he has is 'Student
+        # Employee'. Thus, if we disallowed someone with the 'Student Employee' affilation to sign up
+        # as a teacher, then him and other grad student teachers would not be able to sign up as
+        # teachers to acquire graders. We could not find any other solution to fix this.
         def status_auth
+            # Get the find people osu page and find the search form for looking up students/faculty
             agent = Mechanize.new;
             page = agent.get('https://www.osu.edu/findpeople/');
             form = page.form_with(:id => 'myform');
 
+            # Check to make sure user gave valid @ in email. If they didn't then
+            # we won't be able to get the name.number substring so there is no point
+            # in searching for them in the find people directory 
             atIndex = self.email.index("@");
             if (atIndex.nil?)
-                errors.add(:email, "does not contain a valid name.number");
+                # User gave email input without @ so return and don't search
+                return;
             else
+                # Get the user's name.number. It's possible that they didn't include
+                # a name.number in their email, but this error will be caught below
+                # as well as in the regexp email validation above
                 nameDotNum = self.email[0...atIndex];
-                form.name_n = nameDotNum;
 
+                # Fill in the form with name.number and submit
+                form.name_n = nameDotNum;
                 result_page = form.submit;
     
+                # If the search was successful, we should successfully find an element with classes
+                # 'notification' and 'success', so we know that the user is a real person at OSU.
+                # Also check to make sure the user clicked one of the status radio buttons.
                 if (!result_page.search(".notification.success").empty? && !self.status.nil?)
+
+                    # If the element with class 'results-affilation' includes Student or Employee text
+                    # that corresponds with the Student or Teacher status that the user selected, then
+                    # they are the status that they selected
                     if (result_page.search(".results-affiliation").text.include?(self.status))
                         return true;
+                    # Otherwise, a student is trying to sign up as a teacher, or a teacher is trying
+                    # to sign up as a student, so prevent them from signing up by generating an error
                     else
                         errors.add(:status, "is not valid for given OSU name.number in email");
                     end
+                # If there was not a success message after a search, then the person either gave an
+                # incorrect name.number, or they are not a real person affiliated with OSU, so prevent
+                # them from signing up  by generating an error
                 else
                     errors.add(:email, "does not contain a valid name.number");
                 end
